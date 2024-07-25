6457c3c7ff038bed9a5bfc9669fbc427a5c302ca
@_taeung__ <155827534+ehdnd@users.noreply.github.com>
2024-07-25 15:53:19 +0900

    Maual commit

--- MOMENTUM/js/background.js
+++ MOMENTUM/js/background.js
@@ -2,8 +2,13 @@ const images = ["0.jpeg", "1.jpeg", "2.jpeg"];
 
 const chosenImage = images[Math.floor(Math.random() * images.length)];
 
-const bgImage = document.createElement("img");
+const body = document.querySelector("body");
 
-bgImage.src = `img/${chosenImage}`;
+body.classList.remove();
+body.backgroundImage = `"url(${chosenImage})"`;
 
-document.body.appendChild(bgImage);
+// const bgImage = document.createElement("img");
+
+// bgImage.src = `img/${chosenImage}`;
+
+// document.body.appendChild(bgImage);
